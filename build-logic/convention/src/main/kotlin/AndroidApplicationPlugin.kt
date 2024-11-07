3a50cda0b56ef07f3811d1b2c1b3110d64cdac96
leo-beliakov <leo.beliakov@gmail.com>
2024-11-08 09:56:35 +1100

    Update the version code

--- build-logic/convention/src/main/kotlin/AndroidApplicationPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationPlugin.kt
@@ -28,7 +28,7 @@ class AndroidApplicationPlugin : Plugin<Project> {
                     targetSdk = 34
                     minSdk = 26
                     applicationId = "com.leoapps.eggy"
-                    versionCode = 3
+                    versionCode = 5
                     versionName = "1"
                 }
 
